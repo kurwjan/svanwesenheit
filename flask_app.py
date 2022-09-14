74da7226ea612d4f578efd44cb6002ba77178f66
treewalker187 <developer.baumlaeufer0815@gmail.com>
2022-09-14 22:01:54 +0200

    🔨 Adding login screens, user adding page and a "today" page. But everything is still work in progress.

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+import mysql.connector
+from flask import Flask, render_template, session, redirect, request
+from flask_mysqldb import MySQL
+from flask_session import Session
+from functools import wraps
+from getpass import getpass
 
 app = Flask(__name__)
 
+"""
+app.secret_key = 'deeznuts'
+
+app.config['MYSQL_DATABASE_USER'] = input("Enter username: ")
+app.config['MYSQL_DATABASE_PASSWORD'] = getpass("Enter password: ")
+app.config['MYSQL_DATABASE_DB'] = input("Enter database: ")
+app.config['MYSQL_DATABASE_HOST'] = input("Enter host: ")
+
+db = MySQL(app)
+"""
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/')
-def hello_world():  # put application's code here
+def login():
     return render_template("test.html")
 
 
---
+++ static/scripts/admin-today.js
@@ -0,0 +1,18 @@
+function show(id) {
+    document.getElementById(id).style.display = "block";
+}
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+function add_person(table_id, name, name_id) {
+    let tbody = document.getElementById(table_id).getElementsByTagName('tbody')[0];
+
+    let newRow = tbody.insertRow();
+
+    let newCell = newRow.insertCell();
+
+    let newName = document.getElementById(name).value;
+
+    newCell.outerHTML = "<td>" + newName + "</td><input form='today_form' type='hidden' name=" + name_id + " value=" + newName + ">"
+}
