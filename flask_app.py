ad83f4558cc8526f261fa8a63f0ff64e4f35f986
treewalker187 <developer.baumlaeufer0815@gmail.com>
2022-09-20 22:10:09 +0200

    🔨 Added per user history page, added new features to admin index (today) page, planned "statistics" page merged with user page, new "new login" maybe better must test, added comments and removed many debug prints.

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_session import Session
 from functools import wraps
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -80,28 +81,136 @@ def after_request(response):
     return response
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/user_history", methods=["POST"])
+@login_required
+def user_history():
+    """ Show history of a user """
+
+    user_id = request.form.get("user_id")
+
+    cur = db.connection.cursor()
+
+    # Get name of user
+    cur.execute('SELECT username FROM users WHERE id = %s', [user_id])
+    name = cur.fetchone()
+
+    # Select ids and dates from history table in descending order and set date names to german.
+    cur.execute('SET lc_time_names = "de_DE"')
+    cur.execute('SELECT id, DATE_FORMAT(date, "%W, %e.%c.%y") FROM history ORDER BY date DESC')
+    dates = cur.fetchall()
+
+    # Go through each date from the dates list to add user's history from each date.
+    history = []
+    for date in dates:
+        row = []
+
+        # Select information about the SV day from the current date.
+        cur.execute('SELECT status, reason FROM history_data '
+                    'WHERE history_id = %s AND user_id = %s',
+                    [date[0], user_id])
+        query = cur.fetchone()
+
+        # If current SV day has no information, break for loop
+        if not query:
+            break
+
+        # Summarize the information and append it to history list
+        row.append(date[1])
+        row.append(query)
+        history.append(row)
+
+    cur.close()
+
+    return render_template("/normal/user_history.html", history=history, person=name[0])
+
+
+@app.route("/a", methods=["GET", "POST"])
 @login_required
 @editor_required
-def index():
+def a_index():
+    """
+    ADMIN OR EDITOR REQUIRED
+
+    Add SV day, select status from users, add non-elected persons and submit it to the database.
+    """
+
+    # Get data and send it to database
     if request.method == "POST":
         # Query users
         cur = db.connection.cursor()
         cur.execute('SELECT id FROM users')
         rows = cur.fetchall()
-        cur.close()
 
+        # Get id, status ("Entschuldigt", "Fehlend", "Anwesend") and reason (if "Entschuldigt") from all users.
         persons = []
         for row in rows:
             person = []
+
+            # Append id
             person.append(row[0])
+
             status = request.form.get(str(row[0]))
             person.append(status)
-            if status == "entschuldigt":
+
+            # If Entschuldigt get reason and append
+            if status == "Entschuldigt":
                 person.append(request.form.get("reason-" + str(row[0])))
+
             persons.append(person)
 
-        print(persons)
+        # Get the non-elected person names.
+        non_elected_persons = request.form.getlist("non_name")
+        other_persons = request.form.getlist("other_name")
+
+        # Debug
+        cur.execute('SET TIMESTAMP=unix_timestamp("2022-04-01")')
+
+        # Create new day in database.
+        cur.execute('INSERT INTO history (cancelled) VALUES (0)')
+        db.connection.commit()
+
+        # Debug
+        cur.execute('SELECT id FROM history WHERE date = %s', ["2022-04-01"])
+
+        # Get id of current SV day.
+        # cur.execute('SELECT id FROM history WHERE date = %s', [datetime.today().strftime('%Y-%m-%d')])
+
+        history_id = cur.fetchone()
+
+        # Debug
+        cur.execute('SET TIMESTAMP=unix_timestamp("default")')
+
+        # Create new history log in history_data table with users status, reason (If "Entschuldigt").
+        for person in persons:
+
+            # Get username of current person
+            cur.execute('SELECT username FROM users WHERE id = %s', [person[0]])
+            username = cur.fetchone()
+
+            # If user has a reason, submit it to table.
+            if len(person) == 3:
+                cur.execute('INSERT INTO history_data (person_type, history_id, username, user_id, status, reason)'
+                            ' VALUES ("elected", %s, %s, %s, %s, %s)',
+                            [history_id, username[0], person[0], person[1], person[2]])
+            else:
+                cur.execute(
+                    'INSERT INTO history_data (person_type, history_id, username, user_id, status)'
+                    ' VALUES ("elected", %s, %s, %s, %s)', [history_id, username[0], person[0], person[1]])
+
+        # Create new history log with non-elected person ("Freiwillige Personen") names
+        if non_elected_persons:
+            for person in non_elected_persons:
+                cur.execute('INSERT INTO history_data (person_type, history_id, non_elected_name)'
+                            ' VALUES ("not_elected", %s, %s)', [history_id, person])
+
+        # Create new history log with other person (other people than "Freiwilige Personen") names
+        if other_persons:
+            for person in other_persons:
+                cur.execute('INSERT INTO history_data (person_type, history_id, non_elected_name)'
+                            ' VALUES ("other_person", %s, %s)', [history_id, person])
+
+        db.connection.commit()
+        cur.close()
 
         return redirect("/")
     else:
@@ -115,22 +224,27 @@ def index():
         return render_template("/admin/edit-today.html", persons=rows)
 
 
-@app.route("/create_user", methods=["GET", "POST"])
+@app.route("/a_create_user", methods=["GET", "POST"])
 @login_required
 @admin_required
-def create_user():
-    """ Create new user or manage users """
+def a_create_user():
+    """
+    ADMIN REQUIRED
+
+    Create new user or manage users
+    """
+
     if request.method == "POST":
         username = request.form.get("username")
         usertype = request.form.get("usertype")
+        office = request.form.get("user_office")
         notice = request.form.get("notice")
 
         # Create new user in database
         cur = db.connection.cursor()
-        cur.execute('INSERT INTO users (username, hash, type, blocked, notice) VALUES (%s, "PASSWORD", %s, 0, %s)',
-                    (username, usertype, notice))
-        query = "CREATE TABLE " + username + " (id INT NOT NULL, user_id INT NOT NULL, status TEXT NOT NULL, reason TEXT NULL, PRIMARY KEY (id))"
-        cur.execute(query)
+        cur.execute('INSERT INTO users (username, hash, type, blocked, notice, office, reset) '
+                    'VALUES (%s, "PASSWORD", %s, 0, %s, %s, 1)',
+                    (username, usertype, notice, office))
         db.connection.commit()
         cur.close()
 
@@ -140,10 +254,40 @@ def create_user():
         cur = db.connection.cursor()
         cur.execute('SELECT * FROM users')
         rows = cur.fetchall()
-        print(rows)
+
+        # Get an id from all users
+        cur.execute('SELECT id FROM users')
+        ids = cur.fetchall()
+
+        # Count their status ("Anwesend", "Fehlend", "Entschuldigt")
+        status = []
+        for user_id in ids:
+            row = []
+            cur.execute('SELECT COUNT(status) FROM history_data WHERE user_id = %s AND status = "Anwesend"', [user_id])
+            anwesend = cur.fetchone()
+            row.append(anwesend)
+
+            cur.execute('SELECT COUNT(status) FROM history_data WHERE user_id = %s AND status = "Fehlend"', [user_id])
+            fehlend = cur.fetchone()
+            row.append(fehlend)
+
+            cur.execute('SELECT COUNT(status)  FROM history_data WHERE user_id = %s AND status = "Entschuldigt"',
+                        [user_id])
+            entschuldigt = cur.fetchone()
+
+            row.append(entschuldigt)
+            status.append(row)
+
+        # Count non-elected ("Freiwillige Personen") names
+        cur.execute('SELECT DISTINCT(non_elected_name), COUNT(non_elected_name) '
+                    'FROM history_data WHERE person_type = "not_elected" '
+                    'GROUP BY non_elected_name')
+        not_elected_persons = cur.fetchall()
+
         cur.close()
 
-        return render_template("/admin/admin-users.html", users=rows)
+        return render_template("/admin/admin-users.html", users=rows, status=status,
+                               not_elected_persons=not_elected_persons)
 
 
 @app.route("/delete_user", methods=["POST"])
@@ -154,12 +298,8 @@ def delete_user():
     user_id = request.form.get("user_id")
 
     cur = db.connection.cursor()
-    cur.execute('SELECT username FROM users WHERE id = %s', [user_id])
-    rows = cur.fetchone()
     cur.execute('DELETE FROM users WHERE id = %s', [user_id])
     db.connection.commit()
-    query = "DROP TABLE " + rows[0]
-    cur.execute(query)
     cur.close()
 
     return '', 204
@@ -203,38 +343,38 @@ def login():
             flash("Bitte gebe einen Benutzernamen ein!")
             return render_template("login.html")
 
-        # Ensure password was submitted
-        elif not password:
-            flash("Bitte gebe einen Passwort ein!")
-            return render_template("login.html")
-
         # Query database for username
         cur = db.connection.cursor()
         cur.execute('SELECT * FROM users WHERE username = %s', [username])
-        rows = cur.fetchone()
+        row = cur.fetchone()
         cur.close()
 
         # Check if username exists
-        if not rows:
-            flash("Falscher Benutzername oder falsches Passwort!")
+        if not row:
+            flash("Falscher Benutzername oder ungültiges Passwort!")
             return render_template("login.html")
 
-        # Check if user is a newly created account
-        if rows[2] == "PASSWORD":
-            session["user_id"] = rows[0]
-            session["user_type"] = rows[3]
+        # Check if user is newly created
+        if row[7] == 1:
+            session["user_id"] = row[0]
+            session["user_type"] = row[3]
             return render_template("first-login.html")
 
+        # Ensure password was submitted
+        if not password:
+            flash("Bitte gebe einen Passwort ein!")
+            return render_template("login.html")
+
         # Ensure password is correct
-        if not check_password_hash(rows[2], password):
-            flash("Falscher Benutzername oder falsches Passwort!")
+        if not check_password_hash(row[2], password):
+            flash("Falscher Benutzername oder ungültiges Passwort!")
             return render_template("login.html")
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]
+        session["user_id"] = row[0]
 
         # Remeber user account type
-        session["user_type"] = rows[3]
+        session["user_type"] = row[3]
 
         # Redirect user to home page
         return redirect("/")
